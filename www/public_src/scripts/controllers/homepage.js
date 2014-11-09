885dec85589df294fc4f318f5b5936effc450db9
Thomas J. Fox <thomasjfox1@gmail.com>
2014-11-09 13:58:09 +0100

    added form in html to submit phrase for encryption

--- www/public_src/scripts/controllers/homepage.js
+++ www/public_src/scripts/controllers/homepage.js
@@ -7,23 +7,29 @@ angular.module('enigma')
 
         $scope.colorOptions = ['#002635', '#013440', '#AB1A25', '#D97925'];
 
-        ajaxService.getAppTitle().then(function(results){
-            $scope.mainTitle = results;
-        });
+        // ajaxService.getAppTitle().then(function(results){
+        //     $scope.mainTitle = results;
+        //     $scope.engima = results;
+        // });
 
-        // $scope.changeColor = function(color){
-        //   $scope.titleBGColor = color;
-        // };
+        $scope.phrase = "";
+        $scope.result = [];
 
         //              1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26
         alphabet =    ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
         rotorOne =    ['O','E','S','Q','A','C','J','M','R','I','U','T','Z','W','D','G','K','Y','B','F','X','P','V','L','H','N'];
         rotorTwo =    ['B','T','G','F','O','I','A','R','P','J','Z','S','W','Q','K','E','V','X','D','M','H','L','Y','N','U','C'];
         rotorThree =  ['K','C','N','V','H','R','U','L','F','M','T','I','B','X','Q','A','O','G','D','P','J','S','Z','Y','W','E'];
-        // rotorFour =  'UAQJEGVFMIZWPCLTKDBNSROYHX';
+        rotorFour =   ['U','A','Q','J','E','G','V','F','M','I','Z','W','P','C','L','T','K','D','B','N','S','R','O','Y','H','X'];
         reflector =   ['Q','R','L','A','U','D','O','W','J','E','G','B','H','X','M','Z','N','I','C','T','Y','F','P','V','K','S'];
 
-// LSDAKZN
+        $scope.encrypt = function() {
+            var temp;
+
+            console.log("this is good");
+            temp= phraseEncode($scope.phrase);
+            $scope.result = temp.join("");
+        };
 
         function encode(letter, rotor){
           console.log('yayayay');
@@ -71,14 +77,14 @@ angular.module('enigma')
         // result = encode(result, rotorFirst);
         // console.log(result);
 
-        result = phraseEncode('HELLO WORLD');
-        console.log(result);
+        // result = phraseEncode('HELLO WORLD I AM ALIVE');
+        // console.log(result);
 
 
         // console.log('hello');
 
-        encypt = function(rotorOne, rotorTwo, rotorThree, input, plugboard) {
+        // encypt = function(rotorOne, rotorTwo, rotorThree, input, plugboard) {
 
-        };
+        // };
 
     });
